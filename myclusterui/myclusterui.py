5b956408ab5f6d1eff2ec52d4ece73af0bd2b853
Jamil Appa <jamil.appa@zenotech.com>
2016-05-05 10:01:00 +0100

    pep8 changes

--- myclusterui/myclusterui.py
+++ myclusterui/myclusterui.py
@@ -10,8 +10,10 @@ from PySide.QtGui import (QWidget, QDialog, QListWidget, QListWidgetItem,
                                 QComboBox, QColor, QGridLayout, QTabWidget,
                                 QRadioButton, QButtonGroup, QSplitter,
                                 QStyleFactory, QScrollArea,QApplication,QIcon,
-                                QDoubleSpinBox,QSplashScreen, QPixmap, QStatusBar, QMainWindow )
+                                QDoubleSpinBox,QSplashScreen, QPixmap, 
+                                QStatusBar, QMainWindow )
 from PySide.QtCore import Qt, QSize, SIGNAL, SLOT, Slot, QThread
+
 
 class NoDefault:
     pass
@@ -39,7 +41,7 @@ class ConfiguratorWindow(QMainWindow):
         buttonBox.addButton(self.quitButton, QDialogButtonBox.RejectRole)
         self.connect(self.saveButton, SIGNAL('clicked()'), self.save_file)
         self.connect(self.quitButton, SIGNAL('clicked()'), self.close)
-        
+
         """
         bbox = QDialogButtonBox(QDialogButtonBox.Apply
                                 |QDialogButtonBox.Cancel)
@@ -54,28 +56,39 @@ class ConfiguratorWindow(QMainWindow):
         self.spinboxes = {}
         self.availability_label = None
 
-        self.job_name_widget = self.create_lineedit('Job Name','job_name')
-        self.job_script_widget = self.create_lineedit('Job Script','job_script')
-        self.job_output_widget = self.create_lineedit('Job Output','job_output')
-        self.project_name_widget = self.create_lineedit('Project/Account','project_name')
+        self.job_name_widget = self.create_lineedit('Job Name', 'job_name')
+        self.job_script_widget = self.create_lineedit('Job Script',
+                                                      'job_script')
+        self.job_output_widget = self.create_lineedit('Job Output',
+                                                      'job_output')
+        self.project_name_widget = self.create_lineedit('Project/Account',
+                                                        'project_name')
         self.queue_widget = self.create_combobox('Queue', [], 'queues')
         self.availability_label = QLabel('Available:')
-        self.num_tasks_widget = self.create_spinbox('Number tasks', '', 'ntasks', NoDefault, 1, 1, 1, 'total number of tasks')
-        self.task_per_node_widget = self.create_spinbox('Task per node', '', 'task_per_node', NoDefault, 1, 2, 1, 'tasks per node')
-        self.runtime_widget = self.create_spinbox('Runtime', 'hrs', 'runtime', NoDefault, 1, 36, 1, 'runtime in hrs')
-        self.app_script_widget = self.create_lineedit('Application Script','app_script')
-        
-        
-        hsplitter = QSplitter()
-        #hsplitter.addWidget(self.pages_widget)
+        self.num_tasks_widget = self.create_spinbox('Number tasks',
+                                                    '', 'ntasks',
+                                                    NoDefault, 1, 1, 1,
+                                                    'total number of tasks')
+        self.task_per_node_widget = self.create_spinbox('Task per node',
+                                                        '', 'task_per_node',
+                                                        NoDefault, 1, 2, 1,
+                                                        'tasks per node')
+        self.runtime_widget = self.create_spinbox('Runtime', 'hrs', 'runtime',
+                                                  NoDefault, 1, 36, 1,
+                                                  'runtime in hrs')
+        self.app_script_widget = self.create_lineedit('Application Script',
+                                                      'app_script')
+
+        # hsplitter = QSplitter()
+        # hsplitter.addWidget(self.pages_widget)
 
         btnlayout = QHBoxLayout()
         btnlayout.addStretch(1)
         btnlayout.addWidget(buttonBox)
 
         vlayout = QVBoxLayout()
-        #vlayout.addWidget(hsplitter)
-        
+        # vlayout.addWidget(hsplitter)
+
         vlayout.addWidget(self.job_name_widget)
         vlayout.addWidget(self.job_script_widget)
         vlayout.addWidget(self.job_output_widget)
@@ -93,10 +106,10 @@ class ConfiguratorWindow(QMainWindow):
 
         self.widget = QWidget()
         self.widget.setLayout(vlayout)
-        
+
         self.setCentralWidget(self.widget)
 
-        #self.setGeometry(300, 300, 350, 250)
+        # self.setGeometry(300, 300, 350, 250)
         self.setWindowTitle("MyCluster Job Configurator")
 
         self.lineedits['job_name'].textChanged.connect(self.job_name_changed)
@@ -110,38 +123,39 @@ class ConfiguratorWindow(QMainWindow):
         mycluster.init()
 
         self.init_queue_info()
-        
+
     def save_file(self):
         from mycluster import mycluster
         if mycluster.scheduler:
             index = self.comboboxes['queues'].currentIndex()
             data = self.comboboxes['queues'].itemData(index)
             jobqueue = data.split(' ')[0]
-            
+
             # Add checks
-            
+
             mycluster.create_submit(jobqueue,
-                                        script_name=self.lineedits['job_script'].text()+'.job',
-                                        my_script=self.lineedits['app_script'].text(),
-                                        my_name=self.lineedits['job_name'].text(),
-                                        my_output=self.lineedits['job_output'].text()+'.out',
-                                        num_tasks=self.spinboxes['ntasks'].value(),
-                                        project_name=self.lineedits['project_name'].text(),
-                                        wall_clock=self.spinboxes['runtime'].value(),
-                                        tasks_per_node=self.spinboxes['task_per_node'].value(),)
-        
+                                    script_name=self.lineedits['job_script'].text()+'.job',
+                                    my_script=self.lineedits['app_script'].text(),
+                                    my_name=self.lineedits['job_name'].text(),
+                                    my_output=self.lineedits['job_output'].text()+'.out',
+                                    num_tasks=self.spinboxes['ntasks'].value(),
+                                    project_name=self.lineedits['project_name'].text(),
+                                    wall_clock=self.spinboxes['runtime'].value(),
+                                    tasks_per_node=self.spinboxes['task_per_node'].value(),)
+
     def init_queue_info(self):
         from mycluster import mycluster
         if mycluster.scheduler:
             for q in mycluster.queues():
-                nc = mycluster.scheduler.node_config(q)
+                # nc = mycluster.scheduler.node_config(q)
                 tpn = mycluster.scheduler.tasks_per_node(q)
                 avail = mycluster.scheduler.available_tasks(q)
-                self.comboboxes['queues'].addItem(q+' max task: '+str(avail['max tasks']), q+' '+str(avail['max tasks'])+' '+str(tpn)+ ' '+str(avail['available']))
-                    
+                self.comboboxes['queues'].addItem(q+' max task: '+str(avail['max tasks']),
+                                                  q+' '+str(avail['max tasks'])+' '+str(tpn)+ ' '+str(avail['available']))
+
         self.comboboxes['queues'].currentIndexChanged.connect(self.queue_changed)
         self.queue_changed()
-        
+
     def queue_changed(self):
         if self.comboboxes['queues'].count():
             index = self.comboboxes['queues'].currentIndex()
@@ -150,13 +164,13 @@ class ConfiguratorWindow(QMainWindow):
             self.spinboxes['task_per_node'].setMaximum(int(data.split(' ')[2]))
             self.spinboxes['task_per_node'].setValue(int(data.split(' ')[2]))
             self.availability_label.setText('Available: '+data.split(' ')[3]+' tasks')
-    
-    def job_name_changed(self,text):
-        #print 'job name changed'
+
+    def job_name_changed(self, text):
+        # print 'job name changed'
         self.lineedits['job_script'].setText(text)
         self.lineedits['job_output'].setText(text)
 
-    def job_script_changed(self,text):
+    def job_script_changed(self, text):
         # Check for file exist
         if os.path.isfile(text+'.job'):
             self.statusBar().showMessage('Warning file: '+text+'.job already exists')
@@ -225,7 +239,7 @@ class ConfiguratorWindow(QMainWindow):
         widget = QWidget(self)
         widget.setLayout(layout)
         return widget
-    
+
     def create_combobox(self, text, choices, option, default=NoDefault,
                         tip=None):
         """choices: couples (name, key)"""
@@ -234,8 +248,8 @@ class ConfiguratorWindow(QMainWindow):
         if tip is not None:
             combobox.setToolTip(tip)
         for name, key in choices:
-            combobox.addItem(name, key)#to_qvariant(key))
-        self.comboboxes[option] = combobox 
+            combobox.addItem(name, key)  # to_qvariant(key))
+        self.comboboxes[option] = combobox
         layout = QHBoxLayout()
         for subwidget in (label, combobox):
             layout.addWidget(subwidget)
@@ -245,27 +259,29 @@ class ConfiguratorWindow(QMainWindow):
         widget.setLayout(layout)
         return widget
 
+
 def _resourcepath(directory):
     '''Returns the path to the resource directory inside the package'''
     modulename = os.path.abspath(__file__)
     return os.path.join(os.path.dirname(modulename), directory)
+
 
 def main():
     app = QApplication(sys.argv)
     import qdarkstyle
     # setup stylesheet
     app.setStyleSheet(qdarkstyle.load_stylesheet())
-    pixmap = QPixmap(os.path.join(_resourcepath('images'),"splash.png"))
-    splash = QSplashScreen(pixmap,Qt.WindowStaysOnTopHint)
+    pixmap = QPixmap(os.path.join(_resourcepath('images'), "splash.png"))
+    splash = QSplashScreen(pixmap, Qt.WindowStaysOnTopHint)
     splash.setMask(pixmap.mask())
     splash_font = splash.font()
     splash_font.setPixelSize(14)
     splash.setFont(splash_font)
     splash.show()
     splash.showMessage('Initialising...',
-                           Qt.AlignBottom | Qt.AlignLeft | 
-                           Qt.AlignAbsolute,
-                           Qt.white)
+                       Qt.AlignBottom | Qt.AlignLeft |
+                       Qt.AlignAbsolute,
+                       Qt.white)
     app.processEvents()
     """
     for count in range(1, 6):
@@ -274,9 +290,9 @@ def main():
                            Qt.white)
         QApplication.processEvents()
         QThread.msleep(1000)
-    """  
+    """
     frame = ConfiguratorWindow()
-    
+
     frame.show_and_raise()
     splash.finish(frame)
     sys.exit(app.exec_())
